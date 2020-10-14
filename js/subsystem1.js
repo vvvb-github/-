46c2b4e1aebfb400f02f713c150700c28292ad90
gaoshengyi09018133 <1405205313@qq.com>
2020-10-14 21:05:20 +0800

    参数与运行

--- js/subsystem1.js
+++ js/subsystem1.js
@@ -11,4 +11,28 @@ $(function () {
         $('#box').html("<video id='video' class='plot-img' autoplay loop src='../assets/flash.mp4' type='video/mp4'></video> ");  
         document.getElementById("video").playbackRate=5;
     })
+
+    $('#run-btn').click(()=>{
+        $('#percent').html('0%');
+        $('#bar').width('0');
+        let progress = 0;
+
+        let timer = setInterval(()=>{
+            progress += Math.floor(Math.random()*3);
+            if(progress >= 100) {
+                progress = 100;
+                $('#percent').html(String(progress)+'%');
+                $('#bar').width(String(progress) + '%');
+                clearInterval(timer);
+                calculateOver();
+            }
+            $('#percent').html(String(progress)+'%');
+            $('#bar').width(String(progress) + '%');
+        }, 30);
+    })
 })
+
+function calculateOver() {
+    let filename = $('#file-input')[0].files[0].name;
+    alert(filename);
+}
